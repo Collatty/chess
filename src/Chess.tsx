146cd0528d78d7ce437bc94decf3e9bd05797c15
= <hakoncb@gmail.com>
2021-10-05 22:55:31 +0200

    Add logo

--- src/Chess.tsx
+++ src/Chess.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Board } from './Board';
+import { CollattyChessLogo } from './logo';
 
 export const Chess = () => (
-    <DndProvider backend={HTML5Backend}>
-        <div className="chess">
-            <h1 className="header-primary">Collatty chess</h1>
-            <Board></Board>
+    <>
+        <div className="header">
+            <div className="logo">
+                <CollattyChessLogo></CollattyChessLogo>
+            </div>
         </div>
-    </DndProvider>
+        <DndProvider backend={HTML5Backend}>
+            <div className="chess">
+                <h1 className="header-primary">Collatty chess</h1>
+                <Board></Board>
+            </div>
+        </DndProvider>
+    </>
 );
