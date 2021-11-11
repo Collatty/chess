f146ae97849cb45b672a4abd220d9f65a7e40321
= <hakoncb@gmail.com>
2021-11-11 15:03:45 +0100

    Add move support on mobile

--- src/Chess.tsx
+++ src/Chess.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import { Board } from './Board';
 import { BoardContext, useBoardReducer } from './state/useBoardReducer';
 import type { Action, State } from './types';
@@ -12,6 +13,8 @@ export interface Props {
     highlightLegalMoves?: boolean;
 }
 
+const isMobile = 'ontouchstart' in document.documentElement;
+
 export const Chess = ({
     primaryPlayer,
     autoQueen,
@@ -19,9 +22,10 @@ export const Chess = ({
     highlightLegalMoves,
 }: Props) => {
     const [state, dispatch] = useBoardReducer();
+    console.log(isMobile);
 
     return (
-        <DndProvider backend={HTML5Backend}>
+        <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
             <BoardContext.Provider
                 value={externalStateHandler ?? [state, dispatch]}
             >
